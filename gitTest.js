962dfa1c1f90dce1f1a3a765837e340bbd9e2d11
Hack Reactor Students <accounts@hackreactor.com>
2015-07-16 16:52:46 -0700

    first branch1 commit

--- gitTest.js
+++ gitTest.js
@@ -1,3 +1,8 @@
 // This is the master branch
-// It has been pushed once
-// right now there are no other branches
+// It has been pushed twice
+// right now there is one branch
+
+var a = 2;
+var b = 3;
+var c = a*b;
+console.log(c);
